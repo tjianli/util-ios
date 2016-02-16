b09178892026a0aa56acfb1117e2269d653c1885
tjianli <tjianli@126.com>
2016-02-16 15:48:35 +0800

    init

---
+++ AppcooUtil/AppcooUtil.h
@@ -0,0 +1,19 @@
+//
+//  AppcooUtil.h
+//  AppcooUtil
+//
+//  Created by tjianli on 16/2/16.
+//  Copyright © 2016年 appcoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppcooUtil.
+FOUNDATION_EXPORT double AppcooUtilVersionNumber;
+
+//! Project version string for AppcooUtil.
+FOUNDATION_EXPORT const unsigned char AppcooUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppcooUtil/PublicHeader.h>
+
+
---
+++ AppcooUtil/AppcooUtil.swift
@@ -0,0 +1,9 @@
+//
+//  AppcooUtil.swift
+//  AppcooUtil
+//
+//  Created by tjianli on 16/2/16.
+//  Copyright © 2016年 appcoo. All rights reserved.
+//
+
+import Foundation
